ce0ccf9a103455931e274b2c3a6dfde686a820ab
BiBzz <alexandre.berrebi@businessagile.eu>
2016-04-02 00:26:17 +0200

    Add test error for database connection & set cookie to remember pseudo

--- part3/tp-minichat/minichat.php
+++ part3/tp-minichat/minichat.php
@@ -1,22 +1,44 @@
+<?php
+	setcookie('pseudo','Alex',time()+360*24*3600,null,null,false,true);
+?>
 <!DOCTYPE html>
 <html>
-<head>
 	<title>Minichat</title>
 </head>
 <body>
 	<form method="post" action="minichat_post.php">
-		<label for="pseudo">Pseudo : </label><input type="text" name="pseudo" id="pseudo" required><br/>
+		<?php
+		if (isset($_COOKIE['pseudo']))
+		{
+			echo '<label for="pseudo">Pseudo : </label><input type="text" name="pseudo" id="pseudo" value=' . $_COOKIE['pseudo'] . ' required><br/>';
+		}
+		else
+		{
+			echo '<label for="pseudo">Pseudo : </label><input type="text" name="pseudo" id="pseudo" required><br/>';
+		}
+		?>
 		<label for="message">Message : </label><input type="text" name="message" id="message" required><br/>
 		<input type="submit" name="envoyer" value="Envoyer">
 	</form>
 	<br/>
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=minichat','root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	$reponse = $bdd->query('SELECT * FROM minichat ORDER BY ID DESC LIMIT 10');
+	//Database connection
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=minichat','root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
+	//Select last 10 messages with related pseudo
+	$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 10');
+	//Display each messages with related pseudo
 	while ($donnees = $reponse->fetch())
 	{
 		echo '<p><strong>' . $donnees['pseudo'] . '</strong> : ' . $donnees['message'] . '</p>';
 	}
+	$reponse->closeCursor();
 ?>
 </body>
 </html>
--- part3/tp-minichat/minichat_post.php
+++ part3/tp-minichat/minichat_post.php
@@ -1,9 +1,15 @@
 <?php
+	//Automatic redirection to minichat.php
 	header('Location: minichat.php');
-	if (isset($_POST['pseudo']) && isset($_POST['message']))
-		{
-			$bdd = new PDO('mysql:host=localhost;dbname=minichat','root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$requete = $bdd->prepare('INSERT INTO minichat(pseudo,message) VALUES(:pseudo,:message)');
-			$requete->execute(array('pseudo' => strip_tags($_POST['pseudo']),'message' => strip_tags($_POST['message'])));
-		}
+	//Database connection
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=minichat','root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
+	$requete = $bdd->prepare('INSERT INTO minichat(pseudo,message) VALUES(:pseudo,:message)');
+	$requete->execute(array('pseudo' => strip_tags($_POST['pseudo']),'message' => strip_tags($_POST['message'])));
 ?>
